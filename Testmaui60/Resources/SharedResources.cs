8f81f7856959e69e2f57d5f59dac61bfffd8c2e7
Javier Horacio Fuchs <javier.horacio.fuchs@icloud.com>
2023-07-22 23:51:24 -0300

    SharedResources: Colors

---
+++ Testmaui60/Resources/SharedResources.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Testmaui60
+{
+	public class SharedResources
+	{
+        public static readonly Color Color1 = Color.FromRgb(0, 55, 25); //  color
+        public static readonly Color Color2 = Color.FromRgb(100, 125, 123); //   color
+        public static readonly Color Color3 = Color.FromRgb(25, 10, 30); //   color
+	}
+}
+
