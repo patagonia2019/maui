9441e47419162b5a6dfddf384a4dfb6082145f8d
Javier Horacio Fuchs <javier.horacio.fuchs@icloud.com>
2023-07-22 23:26:08 -0300

    LayoutExample: iOS/Android

--- Testmaui60/LayoutExample.xaml.cs
+++ Testmaui60/LayoutExample.xaml.cs
@@ -2,8 +2,10 @@
 
 public partial class LayoutExample : ContentPage
 {
-	public LayoutExample()
+    public LayoutExample()
 	{
 		InitializeComponent();
-	}
+
+        //VStackLayout.Padding = DeviceInfo.Platform == DevicePlatform.iOS ? new Thickness(30, 10, 30, 10) : new Thickness(100);
+    }
 }
