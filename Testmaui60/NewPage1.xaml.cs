7018521c2bd357159bc4bfb788698b1fcf4ee840
Javier Horacio Fuchs <javier.horacio.fuchs@icloud.com>
2023-07-22 22:54:16 -0300

    Another way

---
+++ Testmaui60/NewPage1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Testmaui60;
+
+public partial class NewPage1 : ContentPage
+{
+    private int count;
+
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        count++;
+        counterLbl.Text = $"Click Count: {count}";
+
+        SemanticScreenReader.Announce(counterLbl.Text);
+    }
+}
